59d525e598380f865e57078b1bc69b5f3e6d21d1
MoatezBen Abdallah <moatez.abdallah@projetas.com.br>
2017-07-25 16:00:52 -0300

    some changes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs = require('fs');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    fs.readFile('clients.json', 'utf8', function (err, data) {
+        if (err) throw err;
+        res.send(data);
+    });
 });
 
 module.exports = router;
